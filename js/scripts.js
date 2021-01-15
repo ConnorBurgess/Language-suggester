a6a20793293aa771fce2c42b52517f0e1bf169bc
Connor Burgess <connorburgesscodes@gmail.com>
2021-01-15 13:45:07 -0800

    add more branching structure to js

--- js/scripts.js
+++ js/scripts.js
@@ -16,12 +16,10 @@ $(document).ready(function() {
     $("#submit-form").submit(function(event) {
     const nameInput = $("input#first-input").val();
     $("#output").empty();
-    $("#no-refunds").empty();
     $("#output").text(nameInput + ", ok let me check this out. Remember: no refunds for happy users!");
     event.preventDefault();
     setTimeout(function(){
       $("#output").text(" Processing...");
-    
     }, 2000); 
     setTimeout(function(){
       const secondAnswer = parseInt($("#second-selection").val());
@@ -29,8 +27,25 @@ $(document).ready(function() {
       const fourthAnswer = parseInt($("#fourth-selection").val());
       const fifthAnswer = parseInt($("#fifth-selection").val());
       const sixthAnswer = parseInt($("#sixth-selection").val());
-      alert(secondAnswer);
-      $("#output").text("So hey, " + nameInput + " you should probably learn Java. // Why do Java programmers have to wear glasses? Because they don’t C#! ");
+      if (secondAnswer === 1 && fourthAnswer === 2) {
+        $("#output").text("Well, " + nameInput + " I'd probably learn C# in your case.");;
+      }
+      else if (secondAnswer === 2 && thirdAnswer === 2) {
+        $("#output").text("So hey, " + nameInput + " you should probably learn Java. // Why do Java programmers have to wear glasses? Because they don’t C!#");
+      }
+      else if (fourthAnswer === 3 && secondAnswer === 2) {
+        $("#output").text("So hey, " + nameInput + " GitHub was started by prominent members of the Ruby community. Maybe check out Ruby?");
+      }
+      else if (fourthAnswer === 2) {
+        $("#output").text("So hey, " + nameInput + " did you know that Microsoft is responsible for C#? I'd probably just learn C# if they are your favorite.");
+      }
+      else if (fourthAnswer === 1) {
+        $("#output").text("So hey, " + nameInput + " Google loves a Python developer. Why not check that out?");
+      }
+      else {
+        $("#output").text("So hey, " + nameInput + " I'm giving you my most default recommendation: learn Pseudocode! Woohoo!");
+      }
+
     }, 5500); 
   });
 
