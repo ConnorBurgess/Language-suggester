ee8561ec377ae541d0cf7aa31785594ec096e7c7
Connor Burgess <connorburgesscodes@gmail.com>
2021-01-15 10:22:40 -0800

    add more form selections to index.html

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,12 @@ $(document).ready(function() {
   $("#firstForm").submit(function(event) {
     console.log("hello");
     $("#output").empty();
-    const someInput = $("input#firstInput").val();
+    const nameInput = $("input#firstInput").val();
     $("#firstQuestion").hide();
-    $("#secondQuestion").fadeIn(2000);
-    $("#output").append("Actually your name isn't really that important..");
+    $("#secondQuestion").fadeIn(4000);
+    $("#thirdQuestion").fadeIn(6000);
+    $("#fourthQuestion").fadeIn(8000);
+    $("#output").text("Hello there " + nameInput + ", your name isn't important at all in determining a programming language. Please direct your eyeballs to more relevant questions below.").fadeIn(2000);
 
     event.preventDefault();
   });
