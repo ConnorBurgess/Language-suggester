14f06d9e6f608164e1d3e8d17dede074ac29fdb0
Connor Burgess <connorburgesscodes@gmail.com>
2021-01-15 12:26:56 -0800

    rename classes and improve formatting

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,23 @@
 $(document).ready(function() {
-  $("#firstForm").submit(function(event) {
+  $("#first-form").submit(function(event) {
     console.log("hello");
     $("#output").empty();
-    const nameInput = $("input#firstInput").val();
-    $("#firstQuestion").hide();
-    $("#secondQuestion").fadeIn(2000);
-    $("#thirdQuestion").fadeIn(2000);
-    $("#fourthQuestion").fadeIn(2000);
-    $("#fifthQuestion").fadeIn(2500);
-    $("#sixthQuestion").fadeIn(2500);
+    const nameInput = $("input#first-input").val();
+    $("#first-question").hide();
+    $("#second-question").fadeIn(2000);
+    $("#third-question").fadeIn(2000);
+    $("#fourth-question").fadeIn(2000);
+    $("#fifth-question").fadeIn(2500);
+    $("#sixth-question").fadeIn(2500);
     $(".submit-col").fadeIn(5000);
     $("#output").text("Hello there " + nameInput + ". Please direct your eyeballs to more relevant questions below.").fadeIn(2000);
     event.preventDefault();
   });
     $("#submit-form").submit(function(event) {
-      $("#output").empty();
-      $("#secondQuestion").hide();
-      $("#thirdQuestion").hide();
-      $("#fourthQuestion").hide();
-      $("#fifthQuestion").hide();
-      $("#sixthQuestion").hide();
-      event.preventDefault();
-
-
+    $("#output").empty();
+    $("#no-refunds").empty();
+    $("#no-refunds").text("Hi " + nameInput + ", please remember the golden rule: no refunds!").fadeIn(10000);
+    event.preventDefault();
 });
 
 });
