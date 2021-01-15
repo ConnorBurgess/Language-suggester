2abf09917cf9b15c65ee4a5d6c1e1baeb0e5d06e
Connor Burgess <connorburgesscodes@gmail.com>
2021-01-15 10:57:53 -0800

    add more form options to index.html

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,8 @@ $(document).ready(function() {
     $("#secondQuestion").fadeIn(4000);
     $("#thirdQuestion").fadeIn(6000);
     $("#fourthQuestion").fadeIn(8000);
+    $("#fifthQuestion").fadeIn(12000);
+    $("#sixthQuestion").fadeIn(12000);
     $("#output").text("Hello there " + nameInput + ", your name isn't important at all in determining a programming language. Please direct your eyeballs to more relevant questions below.").fadeIn(2000);
 
     event.preventDefault();
