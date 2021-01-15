8bb4862428b57c7a67297f6073e9085248bc2379
Connor Burgess <connorburgesscodes@gmail.com>
2021-01-15 09:51:01 -0800

    include and implement googleapi font

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
-$(document).ready(function () {
+$(document).ready(function() {
   $("#firstForm").submit(function(event) {
     console.log("hello");
     $("#output").empty();
     const someInput = $("input#firstInput").val();
     $("#firstQuestion").hide();
     $("#secondQuestion").fadeIn(2000);
-    $("output").text(someInput);
+    $("#output").append("Actually your name isn't really that important..");
 
     event.preventDefault();
   });
